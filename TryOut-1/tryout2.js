4ddfdc70a291ac5d986996e2ed749f5c09c717f1
srsugara <syauqisugara@gmail.com>
2017-03-28 22:42:48 +0700

    task 2

--- TryOut-1/tryout2.js
+++ TryOut-1/tryout2.js
@@ -1,1 +1,17 @@
+const cheerioReq = require("cheerio-req");
 
+cheerioReq("https://refactory.id/", (err, $) => {
+    if(!err){
+      console.log('The result is HTML');
+      console.log('The title is '+$("title").text());
+    }else{
+      console.log('The result is not HTML');
+    }
+
+});
+
+// const tinyreq = require("tinyreq");
+//
+// tinyreq("https://refactory.id/", (err, body) => {
+//     console.log(err || body);
+// });
